b688ae3b13849e2025ee1864d75f05782a11eb73
John Roche <roche.johnjr@gmail.com>
2017-08-21 10:55:21 -0400

    added player subclasses

--- starter_files/lib/player.rb
+++ starter_files/lib/player.rb
@@ -46,36 +46,38 @@ end
 class CautiousPlayer < Player
   def roll_again?
     super && @turn_score < 2
+    # ^^ run code that's up in parent implementation of that method
   end
 end
 
 ## TODO add your own Player subclasses here
 
 class Stops50 < Player
-
+  def stops50(roll)
+    if roll % 2 == 0
+      @turn_over
+    else
+      super roll_again
+    end
+    #if random number is even ( - 100), stop playing
+    # else, keep playing
+  end
 end
 
 class StopsScore < Player
-
+  def stops_score
+    if @turn_score == 3
+      @turn_over
+    else
+      super roll_again
 end
 
 class StopsRolls < Player
-
+  if roll == 11
+    @turn_over
+  else
+    super roll_again
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
